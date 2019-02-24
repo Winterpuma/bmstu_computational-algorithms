701d28fb9791d176667712c0166a6e2b7f9f353d
Winterpuma <32524078+Winterpuma@users.noreply.github.com>
2019-02-24 19:36:33 +0300

    Finish math of multidim interpolation

--- lab_3/lab_3.py
+++ lab_3/lab_3.py
@@ -1,6 +1,8 @@
 # Аппроксимация ф-и, восстановление ф-и по ее дискретному значению
 # Многомерная интерполяция
 
+from math import ceil
+
 def f(x, y):
     return x**2 + y**2
 
@@ -12,13 +14,25 @@ def get_matrix(x_beg, x_h, x_n, y_beg, y_h, y_n):
     z = [[f(i, j) for i in x] for j in y]
     return x, y, z
 
+def print_matrix(x, y, z):
+    print("   y\\x ", end = '')
+    for i in x:
+        print("{:6}".format(i), end = ' ')
+    
+    for i in range(len(y)):
+        print("\n{:6}".format(y[i]), end = ' ')
+        for j in z[i]:
+            print("{:6}".format(j), end = ' ')
+    print('\n')
+            
+
 # Choose n dots nearest to x in a
 def choose_dots(a, n, x):
     a_len = len(a)
-    i_near = min(range(tbl_len), key = lambda i: abs(a[i] - x)) # index of nearest value
+    i_near = min(range(a_len), key = lambda i: abs(a[i] - x)) # index of nearest value
     space_needed = ceil(n / 2)
     
-    if (i_near + space_needed + 1 > tbl_len):
+    if (i_near + space_needed + 1 > a_len):
         i_end = a_len
         i_start = a_len - n
     elif (i_near < space_needed):
@@ -30,16 +44,41 @@ def choose_dots(a, n, x):
 
     return i_start, i_end
 
+# Matrix of differences 
+def get_diff_matr(tbl, n):
+    for i in range(n):
+        tmp = []
+        for j in range(n-i):
+            tmp.append((tbl[i+1][j] - tbl[i+1][j+1]) / (tbl[0][j] - tbl[0][i+j+1]))
+        tbl.append(tmp)
+    return tbl
+
 # n - polynom's power
 def newtons_interpolation(tbl, n, x):
-    tbl = choose_dots(tbl, n + 1, x)
-    matr = get_matr(tbl, n)
+    matr = get_diff_matr(tbl, n)
     tmp = 1
     res = 0
     for i in range(n+1):
         res += tmp * matr[i+1][0]
         tmp *= (x - matr[0][i])
     return res
+
+def multi_interpolation(x, y, z, x_val, y_val, x_n, y_n):
+    ix_beg, ix_end = choose_dots(x, x_n + 1, x_val)
+    iy_beg, iy_end = choose_dots(y, y_n + 1, y_val)
+
+    x = x[ix_beg : ix_end]
+    y = y[iy_beg : iy_end]
+    z = z[iy_beg : iy_end]
+    for i in range(y_n + 1):
+        z[i] = z[i][ix_beg : ix_end]
+
+    #print("Choosen dots:"
+    #print_matrix(x, y, z)
+
+    res = [newtons_interpolation([x, z[i]], x_n, x_val) for i in range(y_n + 1)]
+    return newtons_interpolation([y, res], y_n, y_val)
+           
 
 x_beg = float(input("Input beginning value of x: "))
 x_h = float(input("Input step for x value: "))
@@ -51,7 +90,7 @@ y_N = int(input("Input amount of dots: "))
 
 x, y, z = get_matrix(x_beg, x_h, x_N, y_beg, y_h, y_N)
 print("\nCreated matrix:")
-#print_matrix(x_tbl, y_tbl)
+print_matrix(x, y, z)
 
 x_n = int(input("Input n(x): "))
 x_find = float(input("Input x: "))
@@ -60,10 +99,8 @@ y_n = int(input("Input n(y): "))
 y_find = float(input("Input y: "))
 
 # Results
-'''
-found = interpolate([x_tbl, y_tbl], n, x)
-print("\nInterpolated: ", found)
-print("F(x)        : ", f(x))
-print("Error       : ", abs(f(x) - found), "\n")
-print("Root of this function is: ", interpolate([y_tbl, x_tbl], n, 0))
-'''
+found = multi_interpolation(x, y, z, x_find, y_find, x_n, y_n)
+print("\nInterpolated   : ", found)
+print("F(x, y)        : ", f(x_find, y_find))
+print("Error          : ", abs(f(x_find, y_find) - found), "\n")
+
