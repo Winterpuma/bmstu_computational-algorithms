c427886129d8986216821238ff32401421936eca
Winterpuma <32524078+Winterpuma@users.noreply.github.com>
2019-03-14 22:35:24 +0300

    Add finding of coefficients

--- lab_4/lab_4.py
+++ lab_4/lab_4.py
@@ -19,8 +19,48 @@ def print_table(x, y, ro):
         print("%.4f %.4f %.4f" % (x[i], y[i], ro[i]))
     print()
 
+def print_matr(matr):
+    for i in matr:
+        print(i)
+
 # Вычислить значение
+def root_mean_square(x, y, ro, n): #n - кол-во искомых коэффициентов
+    length = len(x)
+    sum_x_n = [sum([x[i]**j*ro[i] for i in range(length)]) for j in range(n*2 -1)]
+    print(sum_x_n)
+    sum_y_x_n = [sum([x[i]**j*ro[i]*y[i] for i in range(length)]) for j in range(n)]
+    print(sum_y_x_n)
+    matr = [sum_x_n[i:i+n] for i in range(n)]
+    for i in range(n):
+        matr[i].append(sum_y_x_n[i])
+    print_matr(matr)
+    return Gauss(matr)
+
+def Gauss(matr):
+    n = len(matr)
+    # приводим к треугольному виду
+    for k in range(n):
+        for i in range(k+1,n):
+            coeff = -(matr[i][k]/matr[k][k])
+            for j in range(k,n+1):
+                matr[i][j] += coeff*matr[k][j]
+    print("\ntriangled:")
+    print_matr(matr)
+    # находим неизвестные
+    a = [0 for i in range(n)]
+    for i in range(n-1, -1, -1):
+        for j in range(n-1, i, -1):
+            print(a[j]*matr[i][j], i, j)
+            matr[i][n] -= a[j]*matr[i][j]
+        print_matr(matr)
+        print()
+        a[i] = matr[i][n]/matr[i][i]
+    print("a", a)
+    return a
+    
+
 # Отобразить результат
 
-x, y, ro = read_from_file("data.txt")
+x, y, ro = read_from_file("data2.txt")
 print_table(x, y, ro)
+root_mean_square(x, y, ro, 3)
