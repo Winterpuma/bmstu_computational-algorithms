da80ba6470af8e157d0ec01419141856e706decd
Winterpuma <32524078+Winterpuma@users.noreply.github.com>
2019-05-08 10:40:04 +0300

    Add rect intergation

---
+++ lab_5/integration.py
@@ -0,0 +1,12 @@
+def integrate(start, end, function):
+    step = 0.05#(end - start)/20
+    result = 0
+
+    while start <= end:
+        left = function(start)
+        start += step
+        right = function(start)
+
+        result += step*(left + right)/2
+
+    return round(result, 4)
