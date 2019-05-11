3b4385b4b9bd64ed91c37b9a39026de2b8124755
Winterpuma <32524078+Winterpuma@users.noreply.github.com>
2019-05-11 12:43:15 +0300

    Add dichotomy method

--- lab_5/main.py
+++ lab_5/main.py
@@ -1,10 +1,8 @@
 from math import pow, exp, log
-from time import time
-from numpy.linalg import solve
 
 from interpolation import *
 from integration import integrate
-from gauss import solve_lin_system_gauss
+from gauss import solve_lin_system_gauss, Gauss
 
 
 Q_data = [[2000,   4000,   6000,   8000,   10000,  12000,  14000,  16000,  18000,  20000,  22000,  24000,  26000],
@@ -14,8 +12,6 @@ Q_data = [[2000,   4000,   6000,   8000,   10000,  12000,  14000,  16000,  18000
           [11.000, 11.000, 11.000, 11.000, 11.000, 11.000, 11.000, 11.000, 11.000, 11.000, 11.000, 11.000, 11.000],
           [15.000, 15.000, 15.000, 15.000, 15.000, 15.000, 15.000, 15.000, 15.000, 15.000, 15.000, 15.000, 15.000]]
 
-P_mind = 2
-P_maxd = 25
 EPS = 1e-4
 
 Z_c = [0, 1, 2, 3, 4]
@@ -24,10 +20,6 @@ E_c = [12.13, 20.98, 31.00, 45.00]
 def F(curr_p):
     return coeff - 2*integrate(0, 1, lambda z: Nt(T(z), curr_p)*z)
     
-
-def middle(a, b):
-    return (a + b) / 2
-
 
 def T(z):
     return T0 + (Tw - T0)*(z**m)
@@ -39,15 +31,10 @@ def find_d_e(T, gamma):
 
 
 def find_K(T, d_e):
-    K = []
-    for i in range(4):
-        Q_ip1 = interpolate([Q_data[0], Q_data[i+2]], 4, T)
-        Q_i = interpolate([Q_data[0], Q_data[i+1]], 4, T)
-        K_i = 2*2.415*pow(10, -3) * (Q_ip1/Q_i) * pow(T, 3/2) * exp(-(E_c[i]-d_e[i])*11603/T)
+    Q_i = [interpolate([Q_data[0], Q_data[i+1]], 4, T) for i in range(4)]
+    Q_ip1 = [interpolate([Q_data[0], Q_data[i+2]], 4, T) for i in range(4)]
 
-        K.append(K_i)
-
-    return K
+    return [2*2.415*pow(10, -3) * (Q_ip1[i]/Q_i[i]) * pow(T, 3/2) * exp(-(E_c[i]-d_e[i])*11603/T) for i in range(4)]
 
 
 def gamma_func(gamma, T, X):
@@ -57,9 +44,7 @@ def gamma_func(gamma, T, X):
         right_part += ((exp(X[i])*Z_c[i-1]*Z_c[i-1]) /
                        (1+Z_c[i-1]*Z_c[i-1]*gamma/2))
 
-    right_part *= 5.87*pow(10, 10)/pow(T, 3)
-
-    return gamma*gamma - right_part
+    return gamma*gamma - right_part*5.87*pow(10, 10)/pow(T, 3)
 
 
 def find_gamma(st, end, T, X):
@@ -73,6 +58,8 @@ def find_gamma(st, end, T, X):
 
     return (st+end)/2
 
+#def fing
+
 
 def find_max_increment(X, d_X):
     max_inc = abs(d_X[0]/X[0])
@@ -82,86 +69,75 @@ def find_max_increment(X, d_X):
     return max_inc
 
 
-def Nt(T, P):#, X):
-    X = [-1, 3, -1, -20, -20, -20]
-    #X = [10, 10, 10, -1, -1, -1]
+def Nt(T, P):
+    #X = [-1, 3, -1, -20, -20, -20]
+    X = [-1, 3, -1, -15, -25, -35]
 
     while True:
         gamma = find_gamma(0, 3, T, X)
         d_e = find_d_e(T, gamma)
         K = find_K(T, d_e)
 
-        lin_sys_left_side = [[1, -1, 1, 0, 0, 0],
-                             [1, 0, -1, 1, 0, 0],
-                             [1, 0, 0, -1, 1, 0],
-                             [1, 0, 0, 0, -1, 1],
-                             [-exp(X[0]), -exp(X[1]), -exp(X[2]), -exp(X[3]), -exp(X[4]), -exp(X[5])],
-                             [exp(X[0]), 0, -Z_c[1]*exp(X[2]), -Z_c[2]*exp(X[3]), -Z_c[3]*exp(X[4]), -Z_c[4]*exp(X[5])]]
-
         alpha = 0.285*pow(10, -11)*pow(gamma*T, 3)
 
-        lin_sys_right_side = [log(K[0])+X[1]-X[2]-X[0],
-                              log(K[1])+X[2]-X[3]-X[0],
-                              log(K[2])+X[3]-X[4]-X[0],
-                              log(K[3])+X[4]-X[5]-X[0],
-                              exp(X[0])+exp(X[1])+exp(X[2])+exp(X[3])+exp(X[4])+exp(X[5])-alpha-P*7243/T,
-                              Z_c[1]*exp(X[2])+Z_c[2]*exp(X[3])+Z_c[3]*exp(X[4])+Z_c[4]*exp(X[5])-exp(X[0])]
+        system = [[1, -1, 1, 0, 0, 0, log(K[0])+X[1]-X[2]-X[0]],
+                  [1, 0, -1, 1, 0, 0, log(K[1])+X[2]-X[3]-X[0]],
+                  [1, 0, 0, -1, 1, 0, log(K[2])+X[3]-X[4]-X[0]],
+                  [1, 0, 0, 0, -1, 1, log(K[3])+X[4]-X[5]-X[0]],
+                  [-exp(X[0]), -exp(X[1]), -exp(X[2]), -exp(X[3]),
+                   -exp(X[4]), -exp(X[5]), exp(X[0])+exp(X[1])+exp(X[2])+exp(X[3])+exp(X[4])+exp(X[5])-alpha-P*7243/T],
+                  [exp(X[0]), 0, -Z_c[1]*exp(X[2]), -Z_c[2]*exp(X[3]), -Z_c[3]*exp(X[4]), -Z_c[4]*exp(X[5]),
+                   Z_c[1]*exp(X[2])+Z_c[2]*exp(X[3])+Z_c[3]*exp(X[4])+Z_c[4]*exp(X[5])-exp(X[0])]]
 
-        d_X = solve_lin_system_gauss(lin_sys_left_side, lin_sys_right_side)
-        #d_X = solve(lin_sys_left_side, lin_sys_right_side) # ~30% быстрее
+
+        d_X = Gauss(system)
 
         if find_max_increment(X, d_X) < EPS:
             break
 
         for i in range(len(X)):
             X[i] += d_X[i]
-    # print([exp(i) for i in X], gamma)
+            
     return sum([exp(i) for i in X])
 
 
-        
-if __name__ == '__main__':
-    a = 2
-    b = 25
+def middle(a, b):
+    return (a + b) / 2
 
-    X = [-1, 3, -1, -20, -20, -20]
 
-    Pn = 0.5#float(input("Pнач: "))
-    Tn = 300#float(input("Tнач: ")
-    T0 = 3000#8000#float(input("T0: "))
-    Tw = 3000#2000#float(input("Tw: "))
-    m = 6#float(input("m: "))
-
-    coeff = 7243 * (Pn / Tn)
-    time_start = time()
-
+def dichotomy(a, b, eps, func):
     c = middle(a, b)
 
-    f_a = F(a)
-    f_c = F(c)
-    f_b = F(b)
+    f_a = func(a)
+    f_c = func(c)
+    f_b = func(b)
 
-    while abs(f_c) > EPS:
+    while abs(f_c) > eps:
         if (f_a * f_c) <= 0:
             b = c
-            f_b = F(b)
+            f_b = func(b)
         elif (f_b * f_c) <= 0:
             a = c
-            f_a = F(a)
+            f_a = func(a)
         else:
             print("Ошибка. Корень не найден.")
             break
         c = middle(a, b)
-        f_c = F(c)
+        f_c = func(c)
+    return c
+
         
-    print("Result: ", c)
+if __name__ == '__main__':
 
-    time_end = time()
+    Pn = 0.5#float(input("Pнач: "))
+    Tn = 300#float(input("Tнач: ")
+    T0 = 3000#float(input("T0: "))
+    Tw = 3000#float(input("Tw: "))
+    m = 6#float(input("m: "))
 
-    print("Time req: ", time_end - time_start)
+    coeff = 7243 * (Pn / Tn)
 
-    # # print(t(0, T0, Tw, m))
-    # for i in range(1000, 10001, 1000):
-    #     Nt(t(0, i, Tw, m), 5)
-    #     print(t(0, i, Tw, m))'''
+    curr_p = dichotomy(2, 25, EPS, F)
+        
+    print("Result: ", curr_p)
 
